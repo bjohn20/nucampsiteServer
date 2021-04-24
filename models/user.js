429826bceda0f238b36ee67948cc9c8733acacd5
Brandon Johnson <johnson.c.brandon@gmail.com>
2021-04-24 10:27:48 -0400

    Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,14 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
-    type: "string",
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: "string",
-    required: true,
-  },
   admin: {
     type: Boolean,
     default: false,
   },
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
